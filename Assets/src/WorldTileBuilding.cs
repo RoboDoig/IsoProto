964f180f0b2cd7d9c103317c5de5f3132c889ab1
Andrew Erskine <erskine94@gmail.com>
2020-04-03 17:49:36 -0700

    Added building tile type

---
+++ Assets/src/WorldTileBuilding.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Building Tile", menuName = "Tiles/Building Tile")]
+public class WorldTileBuilding : WorldTile
+{
+    public override List<GoapAction> GenerateActionList()
+    {
+        List<GoapAction> actionList = new List<GoapAction>();
+
+        return actionList;
+    }
+}
